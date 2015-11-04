999fc054acea23ac1526ca50d4d0bf1b47e61c83
Ben O'Steen <bosteen@gmail.com>
2015-11-04 12:29:35 +0000

    OAIPMH gathering complete.

--- src/apihelpers.py
+++ src/apihelpers.py
@@ -4,8 +4,13 @@ from lxml import etree as ET
 
 ONS = "http://www.openarchives.org/OAI/2.0/"
 MARCXML = "http://www.loc.gov/MARC21/slim"
+RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+BL = "http://www.bl.uk/schemas/digitalobject/entities#"
+RTS = "http://cosimo.stanford.edu/sdr/metsrights/"
+PREMIS = "info:lc/xmlns/premis-v2"
 
-ns = {"o": ONS, "m": MARCXML}
+ns = {"o": ONS, "m": MARCXML, 'rdf': RDF,
+      'bl': BL, 'rts': RTS, 'premis': PREMIS}
 
 def oai(text):
   return "{" + ONS + "}" + text
@@ -15,24 +20,34 @@ def mxl(text):
 
 def get_marc_xml(identifier):
   try:
-    payload = {'verb': 'GetRecord', 'metadataPrefix': 'marcxml', 'identifier': '016959122 '}
+    payload = {'verb': 'GetRecord', 'metadataPrefix': 'marcxml', 'identifier': identifier}
     r = requests.get("http://v8b-bldgen01.ad.bl.uk/oaipmh/service", params=payload)
     if r.status_code == 200:
-      return r.text
+      return r.content
     else:
       print(r.status_code)
   except requests.exceptions.ConnectionError as e:
     print("Failed to connect to service.")
     raise e
 
-def parse_oaipmh(xml):
+def get_mers_data(logicalark, dtype = "Structural"):
+  try:
+    payload = {'ark': logicalark, 'type': dtype}
+    r = requests.get("http://mer.ad.bl.uk/mer_access/metadata", params=payload)
+    if r.status_code == 200:
+      return r.content
+    else:
+      print(r.status_code)
+  except requests.exceptions.ConnectionError as e:
+    print("Failed to connect to service.")
+    raise e
+
+def parse(xml):
   try:
     doc = ET.fromstring(xml)
   except ET.XMLSyntaxError as e:
-    print("Bad XML from OAIPMH server")
+    print("Bad XML")
     raise e
-  # root/GetRecord/record/metadata/record/datafield[@tag=876]
-  # ARK is in subfield a
   return doc
 
 def get_ark_from_doc(doc):
